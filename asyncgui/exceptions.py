67adbbd54b402d652ac00534f3bf69f2a0a5eac4
gottadiveintopython <flow4re2c@gmail.com>
2021-06-11 19:09:27 +0900

    import several exceptions from Trio project

--- asyncgui/exceptions.py
+++ asyncgui/exceptions.py
@@ -1,6 +1,7 @@
 __all__ = (
     'InvalidStateError', 'MultiError', 'EndOfConcurrency',
-    'NoChildLeft',
+    'NoChildLeft', 'WouldBlock', 'BusyResourceError',
+    'ClosedResourceError', 'BrokenResourceError', 'EndOfChannel',
 )
 from ._multierror import MultiError
 
@@ -15,3 +16,49 @@ class EndOfConcurrency(BaseException):
 
 class NoChildLeft(Exception):
     """There is no child to wait for"""
+
+
+class WouldBlock(Exception):
+    """(took from trio)
+    Raised by X_nowait functions if X would block.
+    """
+
+
+class BusyResourceError(Exception):
+    """(took from trio)
+    """
+
+
+class ClosedResourceError(Exception):
+    """(took from trio)
+    Raised when attempting to use a resource after it has been closed.
+
+    Note that "closed" here means that *your* code closed the resource,
+    generally by calling a method with a name like ``close`` or ``aclose``, or
+    by exiting a context manager. If a problem arises elsewhere – for example,
+    because of a network failure, or because a remote peer closed their end of
+    a connection – then that should be indicated by a different exception
+    class, like :exc:`BrokenResourceError` or an :exc:`OSError` subclass.
+    """
+
+
+class BrokenResourceError(Exception):
+    """(took from trio)
+    Raised when an attempt to use a resource fails due to external
+    circumstances.
+
+    For example, you might get this if you try to send data on a stream where
+    the remote side has already closed the connection.
+
+    You *don't* get this error if *you* closed the resource – in that case you
+    get :class:`ClosedResourceError`.
+
+    This exception's ``__cause__`` attribute will often contain more
+    information about the underlying error.
+    """
+
+
+class EndOfChannel(Exception):
+    """(took from trio)
+    This is analogous to an "end-of-file" condition, but for channels.
+    """
