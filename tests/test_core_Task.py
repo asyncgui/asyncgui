25626bea82b1e567cc311452d7ec2bb4c0274b3b
Nattōsai Mitō <flow4re2c@gmail.com>
2023-03-21 09:28:11 +0900

    stop importing third party libraries at the top level on unittest

--- tests/test_core_Task.py
+++ tests/test_core_Task.py
@@ -1,13 +1,15 @@
 import pytest
-import asyncgui as ag
-TS = ag.TaskState
 
 
 def test_task_state_ended():
+    from asyncgui import TaskState as TS
     assert TS.ENDED is (TS.CANCELLED | TS.DONE)
 
 
 def test_the_state_and_the_result():
+    import asyncgui as ag
+    TS = ag.TaskState
+
     task_state = 'A'
     async def async_fn():
         nonlocal task_state
@@ -41,6 +43,9 @@ def test_the_state_and_the_result():
 
 
 def test_the_state_and_the_result__ver_cancel():
+    import asyncgui as ag
+    TS = ag.TaskState
+
     task_state = 'A'
     async def async_fn():
         nonlocal task_state
@@ -75,6 +80,9 @@ def test_the_state_and_the_result__ver_cancel():
 
 def test_the_state_and_the_result__ver_uncaught_exception():
     '''例外が自然発生した場合'''
+    import asyncgui as ag
+    TS = ag.TaskState
+
     task_state = 'A'
     async def async_fn():
         nonlocal task_state
@@ -112,6 +120,9 @@ def test_the_state_and_the_result__ver_uncaught_exception():
 
 def test_the_state_and_the_result__ver_uncaught_exception_2():
     '''coro.throw()によって例外を起こした場合'''
+    import asyncgui as ag
+    TS = ag.TaskState
+
     task_state = 'A'
     async def async_fn():
         nonlocal task_state
@@ -148,6 +159,7 @@ def test_the_state_and_the_result__ver_uncaught_exception_2():
 
 def test_throw_exc_to_unstarted_task():
     import asyncgui as ag
+
     task = ag.Task(ag.sleep_forever(), name='pytest')
     assert task.state is ag.TaskState.CREATED
     with pytest.raises(ag.InvalidStateError):
@@ -157,6 +169,7 @@ def test_throw_exc_to_unstarted_task():
 
 def test_throw_exc_to_cancelled_task():
     import asyncgui as ag
+
     task = ag.start(ag.Event().wait())
     assert task.state is ag.TaskState.STARTED
     task.cancel()
@@ -167,6 +180,7 @@ def test_throw_exc_to_cancelled_task():
 
 def test_throw_exc_to_finished_task():
     import asyncgui as ag
+
     e = ag.Event()
     task = ag.start(e.wait())
     assert task.state is ag.TaskState.STARTED
@@ -178,6 +192,7 @@ def test_throw_exc_to_finished_task():
 
 def test_throw_exc_to_started_task_and_get_caught():
     import asyncgui as ag
+
     async def async_fn():
         try:
             await ag.sleep_forever()
@@ -193,6 +208,8 @@ def test_throw_exc_to_started_task_and_get_caught():
 
 @pytest.mark.parametrize('do_suppress', (True, False, ), )
 def test_suppress_exception(do_suppress):
+    import asyncgui as ag
+
     async def async_fn():
         raise ZeroDivisionError
     task = ag.Task(async_fn(), name='pytest')
@@ -204,7 +221,7 @@ def test_suppress_exception(do_suppress):
         with pytest.raises(ZeroDivisionError):
             ag.start(task)
         assert task._exception is None
-    assert task.state is TS.CANCELLED
+    assert task.state is ag.TaskState.CANCELLED
 
 
 def test_cancel_self():
@@ -226,6 +243,7 @@ def test_cancel_self():
 def test_cancel_without_start():
     from inspect import getcoroutinestate, CORO_CLOSED
     import asyncgui as ag
+
     task = ag.Task(ag.sleep_forever())
     task.cancel()
     assert task.cancelled
@@ -248,6 +266,7 @@ def test_try_to_cancel_self_but_no_opportunity_for_that():
 def test_weakref():
     import weakref
     import asyncgui as ag
+
     task = ag.Task(ag.sleep_forever())
     weakref.ref(task)
     task.cancel()
